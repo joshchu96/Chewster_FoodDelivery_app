fe0723253ec450cdd6c339424a5dcad99ecac699
Josh Chu <joshchu96@gmail.com>
2024-07-17 15:42:37 -0700

    update frontend placeorder

--- Frontend/src/pages/PlaceOrder/PlaceOrder.jsx
+++ Frontend/src/pages/PlaceOrder/PlaceOrder.jsx
@@ -1,29 +1,100 @@
-import React, { useContext } from "react";
+import React, { useContext, useState, useEffect } from "react";
 import "./PlaceOrder.css";
-import { ChewsterContext } from "../../context/ChewsterContext";
+import { ChewsterContext } from "../../context/ChewsterContext.jsx";
 
 const PlaceOrder = () => {
-  const { calcCartTotal, deliveryFee } = useContext(ChewsterContext);
+  const { calcCartTotal, deliveryFee, token, food_list, cartItems, BASE_URL } =
+    useContext(ChewsterContext);
+
+  const [data, setData] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    street: "",
+    city: "",
+    state: "",
+    zipcode: "",
+    country: "",
+    phone: "",
+  });
+
+  const handleOnChange = (e) => {
+    const name = e.target.name;
+    const value = e.target.value;
+    setData((prevData) => ({ ...prevData, [name]: value }));
+  };
+
+  useEffect(() => {
+    console.log(data);
+  }, [data]);
 
   return (
     <form action="" className="order-wrapper">
       <div className="order-left">
         <p className="title">Order Delivery Information</p>
         <div className="order-input-fields">
-          <input type="text" placeholder="First Name" />
-          <input type="text" placeholder="Last Name" />
+          <input
+            name="firstName"
+            onChange={handleOnChange}
+            value={data.firstName}
+            type="text"
+            placeholder="First Name"
+          />
+          <input
+            name=" lastName"
+            onChange={handleOnChange}
+            value={data.lastName}
+            type="text"
+            placeholder="Last Name"
+          />
         </div>
-        <input type="email" placeholder="Email" />
-        <input type="text" placeholder="Address" />
+        <input
+          name="email"
+          onChange={handleOnChange}
+          value={data.email}
+          type="email"
+          placeholder="Email"
+        />
+        <input
+          name="street"
+          onChange={handleOnChange}
+          value={data.street}
+          type="text"
+          placeholder="Street Address"
+        />
         <div className="order-input-fields">
-          <input type="text" placeholder="City" />
-          <input type="text" placeholder="State" />
+          <input
+            name="city"
+            onChange={handleOnChange}
+            value={data.city}
+            type="text"
+            placeholder="City"
+          />
+          <input
+            name="state"
+            onChange={handleOnChange}
+            value={data.state}
+            type="text"
+            placeholder="State"
+          />
         </div>
         <div className="order-input-fields">
-          <input type="text" placeholder="Zip Code" />
+          <input
+            name="country"
+            onChange={handleOnChange}
+            value={data.country}
+            type="text"
+            placeholder="Zip Code"
+          />
           <input type="text" placeholder="Country" />
         </div>
-        <input type="text" placeholder="Phone Number" />
+        <input
+          name="phone"
+          onChange={handleOnChange}
+          value={data.phone}
+          type="text"
+          placeholder="Phone Number"
+        />
       </div>
       <div className="order-right">
         <div className="cart-total-wrapper">
